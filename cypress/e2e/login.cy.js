8af43bf350d2507bf35500cf747cac6eff55841d
jose.bianchini <jose.bianchini@siplan.com>
2026-07-20 23:18:55 -0300

    Adicionei o beforeEach para o acesso a pagina de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,26 +1,26 @@
 describe('Login', () => {
-  it('Login com dados válidos deve permitir entrada no sistema', () => {
+  beforeEach(() => {
     // Arrange
     cy.visit('http://localhost:4000')
+  })
 
+  it('Login com dados válidos deve permitir entrada no sistema', () => {
     // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
 
     // Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
-  it.only('Login com dados inválidos deve apresentar mensagem de erro', () => {
-    // Arrange
-    cy.visit('http://localhost:4000')
-
+  it('Login com dados inválidos deve apresentar mensagem de erro', () => {
     // Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('654321')
     cy.get('#login-section > .btn').click()
 
     // Assert
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
   })
 })
